b9724c2d13129f541b1d01ff44915bca5aadd5f4
Leana Lopez <chileana_go@yahoo.com>
2021-09-25 19:07:32 -0700

    added cors

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
 const productsRouter = require("./products/products.router");
-// const categoriesRouter = require("./categories/categories.router");
+const categoriesRouter = require("./categories/categories.router");
 const suppliersRouter = require("./suppliers/suppliers.router");
 
 app.use(morgan("dev"));
+app.use(cors());
 app.use(express.json());
 
 app.use("/products", productsRouter);
+app.use("/categories", categoriesRouter);
 app.use("/suppliers", suppliersRouter);
 
 // Not found handler
